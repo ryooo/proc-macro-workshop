c9a95ee57c8c83ed625c01348abccfe631f7a921
ryooo <ryooo.321@gmail.com>
2022-09-10 18:15:30 +0900

    pass 2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,29 @@
 use proc_macro::TokenStream;
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
-pub fn derive_builder(_input: TokenStream) -> TokenStream {
-    TokenStream::new()
+pub fn derive_builder(input: TokenStream) -> TokenStream {
+    let input = &parse_macro_input!(input as DeriveInput);
+    let base_ident = &input.ident;
+    let builder_ident = format_ident!("{}Builder", base_ident);
+    let token = quote! {
+        pub struct #builder_ident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+        impl #base_ident {
+            pub fn builder() -> #builder_ident {
+                #builder_ident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    };
+    token.into()
 }
