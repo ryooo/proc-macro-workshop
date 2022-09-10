6c657489dd1c29d5750492cc09bedc35ca1de742
ryooo <ryooo.321@gmail.com>
2022-09-10 18:26:00 +0900

    pass 3

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -23,6 +23,26 @@ pub fn derive_builder(input: TokenStream) -> TokenStream {
                     current_dir: None,
                 }
             }
+
+        }
+
+        impl #builder_ident {
+            fn executable(&mut self, executable: String) -> &mut Self {
+                self.executable = Some(executable);
+                self
+            }
+            fn args(&mut self, args: Vec<String>) -> &mut Self {
+                self.args = Some(args);
+                self
+            }
+            fn env(&mut self, env: Vec<String>) -> &mut Self {
+                self.env = Some(env);
+                self
+            }
+            fn current_dir(&mut self, current_dir: String) -> &mut Self {
+                self.current_dir = Some(current_dir);
+                self
+            }
         }
     };
     token.into()
