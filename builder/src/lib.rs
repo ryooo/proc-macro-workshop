e1ede4b57b36cfe23ae27ad52ab8d6ab5db322ce
ryooo <ryooo.321@gmail.com>
2022-09-11 01:19:13 +0900

    fix for flexible fields

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,55 +1,53 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, DeriveInput, Data, Fields};
 
 #[proc_macro_derive(Builder)]
 pub fn derive_builder(input: TokenStream) -> TokenStream {
     let input = &parse_macro_input!(input as DeriveInput);
-    dbg!(input);
     let base_ident = &input.ident;
     let builder_ident = format_ident!("{}Builder", base_ident);
+    let mut fields_info = vec![];
+    let mut types_info = vec![];
+    match &input.data {
+        Data::Struct(data) => {
+            match &data.fields {
+                Fields::Named(fields) => {
+                    for f in fields.named.clone() {
+                        fields_info.push(f.ident.unwrap());
+                        types_info.push(f.ty);
+                    };
+                }
+                _ => panic!("fields should be Named")
+            }
+        }
+        _ => {
+            panic!("data should be Struct")
+        }
+    }
     let token = quote! {
         pub struct #builder_ident {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
+            #(#fields_info: Option<#types_info>,)*
         }
         impl #base_ident {
             pub fn builder() -> #builder_ident {
                 #builder_ident {
-                    executable: None,
-                    args: None,
-                    env: None,
-                    current_dir: None,
+                    #(#fields_info: None,)*
                 }
             }
 
         }
 
         impl #builder_ident {
-            fn executable(&mut self, executable: String) -> &mut Self {
-                self.executable = Some(executable);
-                self
-            }
-            fn args(&mut self, args: Vec<String>) -> &mut Self {
-                self.args = Some(args);
-                self
-            }
-            fn env(&mut self, env: Vec<String>) -> &mut Self {
-                self.env = Some(env);
-                self
-            }
-            fn current_dir(&mut self, current_dir: String) -> &mut Self {
-                self.current_dir = Some(current_dir);
-                self
-            }
+            #(
+                fn #fields_info(&mut self, #fields_info: #types_info) -> &mut Self {
+                    self.#fields_info = Some(#fields_info);
+                    self
+                }
+            )*
             pub fn build(&mut self) -> Result<#base_ident, Box<dyn error::Error>> {
                 Ok(#base_ident {
-                    executable: self.executable.clone().unwrap(),
-                    args: self.args.clone().unwrap(),
-                    env: self.env.clone().unwrap(),
-                    current_dir: self.current_dir.clone().unwrap(),
+                    #(#fields_info: self.#fields_info.clone().unwrap(),)*
                 })
             }
         }
